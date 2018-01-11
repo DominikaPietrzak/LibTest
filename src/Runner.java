36c1748067033881f9e4cc9762d2f47c492590a1
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-01-11 17:43:17 +0100

    some methods was made

--- src/Runner.java
+++ src/Runner.java
@@ -3,36 +3,55 @@
  */
 import  java.util.Scanner;
 import java.util.LinkedList;
+
 public class Runner {
 
 
     public static void main(String[] args) {
 
+        //LinkedList<Book> books = new LinkedList<Book>();
+        //Scanner readOut = new Scanner(System.in);
+        //TODO: after entering quit !!,no new book is added
+        //TODO: add, a function that allows you to add new books to the old list so that they will not be overwritten
+
+        addBook();
+        getAllBooks(addBook());
+
+    }
+
+    public static LinkedList addBook() {
+
         LinkedList<Book> books = new LinkedList<Book>();
         Scanner readOut = new Scanner(System.in);
-
-
         String bookTitle;
         String author;
         do {
             System.out.println("Podaj tytuł");
             bookTitle = readOut.nextLine();
 
-            System.out.println("Podaj autora");
+            if (bookTitle.equals("quit!!")) {
+                break;
+            }
 
+            System.out.println("Podaj autora");
             author = readOut.nextLine();
 
             books.add(new Book(bookTitle, author));
 
+            return books;
         }
 
-        while (!bookTitle.equals("quit!!"));
+        while (true);
+
+        return books;
+    }
+
+    public static void getAllBooks(LinkedList books){
 
         for(int i=0; i<books.size();i++){
             System.out.println(books.get(i).getAuthor());
             System.out.println(books.get(i).getTitle());
 
         }
-
     }
 }
